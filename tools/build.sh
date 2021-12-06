a9a26f573ee1fcb95ad15ab4a33d7b698f178600
Sam Culley <sam@samculley.co.uk>
2021-12-06 09:08:44 +0000

    adding comments to echo line

--- tools/build.sh
+++ tools/build.sh
@@ -25,7 +25,7 @@ done
 
 if [ -f "$BUILD_ARG_FILE" ]; then
   BUILD_ARGS=$(cat $BUILD_ARG_FILE | sed 's/^/--build-arg /g' | paste -s -d " ")
-  echo docker build -t '${IMAGE}' ${BUILD_ARGS} .
+  echo "docker build -t ${IMAGE} ${BUILD_ARGS} ."
 else
   echo "docker push ${IMAGE} ."
 fi
